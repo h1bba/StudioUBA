270645e2dfe3b57940b7fe20078746a700ea7f88
Ufuk <hibbayt@gmail.com>
2024-10-14 18:37:14 +0200

    update font

--- script.js
+++ script.js
@@ -137,3 +137,21 @@ spans.forEach((span, index) => {
         });
     });
 });
+
+
+// LENIS SCROLL BASIC
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+    console.log(e);
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
